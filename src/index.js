88eec6c0505c8be784eaae8921af0c2960e823b4
Zhou Yuhang <zyuhang255@gmail.com>
2022-07-07 19:20:52 +0800

    added future improvements

--- src/index.js
+++ src/index.js
@@ -4,3 +4,8 @@ import App from "./App";
 import "./style.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+// make background image fill whole height in small windows
+// keep display section present even without text inside
+// add on/off button
+// add loudness slider
+// add piano tones
